c8033a118693e27a6b11bc717f1cd196e25ff8a8
fumisan <fumitaka.murakami@levelenter.com>
2021-09-22 15:30:03 +0900

    frist commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+createApp(App)
+  .use(router)
+  .mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        productName: "my-application",
+        appId: "com.sample.myapplication",
+        win: {
+          icon: "./resource/LEicon.png",
+          target: [
+            {
+              target: "nsis", // 'zip', 'nsis', 'portable'
+              arch: ["x64"], // 'x64', 'ia32'
+            },
+          ],
+        },
+      },
+    },
+  },
+};
